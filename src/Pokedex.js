0590c7dd6da4fafe06b15e2b80583140ebc82178
MariahCasciola <117303840+MariahCasciola@users.noreply.github.com>
2023-01-19 16:49:14 -0600

    made api call to pokedex

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,14 +1,29 @@
 import React, { useEffect, useState } from "react";
 
 function Pokedex() {
-  const [pokemon, setPokemon] = useState();
+  //IMPORTANT PART (2) ***************************************
+  const [pokemon, setPokemon] = useState([]);
 
   //keeps track of side effects like api calls
   useEffect(() => {
     const abortController = new AbortController();
-    fetch(`https://pokeapi.co/api/v2/pokemon?limit=10&offset=0`, {
-      signal: abortController.signal,
-    });
+
+    async function apiCall() {
+      try {
+        const response = await fetch(
+          `https://pokeapi.co/api/v2/pokemon?limit=10&offset=0`,
+          { signal: abortController.signal }
+        );
+        //IMPORTANT PART (1) ***************************************
+        const pokemonList = await response.json();
+        setPokemon(pokemonList.results);
+        console.log(pokemonList.results);
+      } catch {
+        throw console.log("error");
+      }
+    }
+    //do I need a parameter?
+    apiCall();
   }, []);
 
   return <div></div>;
