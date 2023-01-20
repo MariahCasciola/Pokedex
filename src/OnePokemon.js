bde5ea9d0bc329ee69c423218ae2dd61978e60e3
MariahCasciola <117303840+MariahCasciola@users.noreply.github.com>
2023-01-20 14:24:20 -0600

    print stats in console on every click of pokemon names on webpage

--- src/OnePokemon.js
+++ src/OnePokemon.js
@@ -1,8 +1,31 @@
-import React from "react";
-import Pokedex from "./Pokedex";
+import React, { useState, useEffect } from "react";
 
-function OnePokemon({ element }) {
-  return <li> {element.name} </li>;
+function OnePokemon({ pokemon }) {
+  const [pokemonStats, setPokemonStats] = useState();
+
+  const clickHandler = ({ target }) => {
+    console.log(pokemonStats);
+  };
+
+  useEffect(() => {
+    const abortController = new AbortController();
+
+    async function fetchThisPokemonStats() {
+      try {
+        const response = await fetch(pokemon.url, {
+          signal: abortController.signal,
+        });
+        //IMPORTANT PART (1) ***************************************
+        const pokemonInfo = await response.json();
+        setPokemonStats(pokemonInfo.stats);
+      } catch {
+        throw console.error("error");
+      }
+    }
+    fetchThisPokemonStats();
+  }, []);
+
+  return <li onClick={clickHandler}> {pokemon.name} </li>;
 }
 
 export default OnePokemon;
--- src/Pokedex.js
+++ src/Pokedex.js
@@ -3,13 +3,13 @@ import OnePokemon from "./OnePokemon";
 
 function Pokedex() {
   //IMPORTANT PART (2) ***************************************
-  const [pokemon, setPokemon] = useState([]);
+  const [pokemonList, setPokemonList] = useState([]);
   // console.log(pokemon)
   //keeps track of side effects like api calls
   useEffect(() => {
     const abortController = new AbortController();
 
-    async function apiCall() {
+    async function fetchFirstTenPokemon() {
       try {
         const response = await fetch(
           `https://pokeapi.co/api/v2/pokemon?limit=10&offset=0`,
@@ -17,19 +17,20 @@ function Pokedex() {
         );
         //IMPORTANT PART (1) ***************************************
         const pokemonList = await response.json();
-        setPokemon(pokemonList.results);
+        setPokemonList(pokemonList.results);
         // console.log(pokemonList.results);
       } catch {
         throw console.log("error");
       }
     }
-    apiCall();
+    fetchFirstTenPokemon();
   }, []);
 
   return (
     <ul>
-      {pokemon.map((element, index) => {
-        return <OnePokemon key={index} element={element} />;
+      {pokemonList.map((pokemon, index) => {
+        // console.log(pokemon)
+        return <OnePokemon key={index} pokemon={pokemon} />;
       })}
     </ul>
   );
